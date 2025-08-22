a44d6e541f48569d4ea69cbd946383e9ebbccf63
Suhas Venkata <113611352+sUhAs1011@users.noreply.github.com>
2025-08-22 17:40:51 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -530,7 +530,7 @@ st.markdown("#### 🌐 Web Development")
 webdev_icon_map = {
     "HTML": "https://img.icons8.com/?size=100&id=20909&format=png&color=000000",
     "CSS": "https://img.icons8.com/?size=100&id=21278&format=png&color=000000",
-    "JavaScript": "https://img.icons8.com/?size=100&id=108784&format=png&color=000000",
+    "JavaScript": ""https://www.vecteezy.com/png/27127463-javascript-logo-png-javascript-icon-transparent-png"",
     "Streamlit": "https://img.icons8.com/?size=100&id=Rffi8qeb2fK5&format=png&color=000000",
     "SpringBoot": "https://img.icons8.com/?size=100&id=90519&format=png&color=000000"
 }
