0b5a2ec9a343a1acdd0cc7c30057b84a90237cd3
Suhas Venkata <113611352+sUhAs1011@users.noreply.github.com>
2025-08-22 17:27:16 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -590,10 +590,11 @@ ml_icon_map = {
     "PyTorch":       "https://img.icons8.com/?size=100&id=jH4BpkMnRrU5&format=png&color=000000",
     "Mathplotlib":   "https://img.icons8.com/?size=100&id=TkX1totjFmAD&format=png&color=000000",
     "Keras": "https://img.icons8.com/?size=100&id=XcSgtbIpgK6W&format=png&color=000000",
-    "Seaborn": "https://cdn.worldvectorlogo.com/logos/seaborn-1.svg"
+    "Seaborn": "https://cdn.worldvectorlogo.com/logos/seaborn-1.svg",
+    "Selenium:" "https://img.icons8.com/?size=100&id=38553&format=png&color=000000"
 }
 
-ml_tools = ["Scikit-learn", "Pandas", "NumPy", "NLTK", "Spacy", "PyTorch", "Mathplotlib", "Keras", "Seaborn"]
+ml_tools = ["Scikit-learn", "Pandas", "NumPy", "NLTK", "Spacy", "PyTorch", "Mathplotlib", "Keras", "Seaborn", "Selenium"]
 ml_cols = st.columns(3)
 
 for i, tool in enumerate(ml_tools):
