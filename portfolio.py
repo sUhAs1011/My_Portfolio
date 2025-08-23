95de2c7f07261192a04648d708ea46572d8c8c39
Suhas Venkata <113611352+sUhAs1011@users.noreply.github.com>
2025-08-23 21:52:13 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -362,7 +362,7 @@ col1, col2 = st.columns([3, 2])
 with col1:
     st.markdown("<div class='hero-container'>", unsafe_allow_html=True)
     st.markdown("<div class='gradient-text'><span class='letter'>S</span><span class='letter'>u</span><span class='letter'>h</span><span class='letter'>a</span><span class='letter'>s</span> <span class='letter'>V</span><span class='letter'>e</span><span class='letter'>n</span><span class='letter'>k</span><span class='letter'>a</span><span class='letter'>t</span><span class='letter'>a</span> <span>👋</span></div>", unsafe_allow_html=True)
-    st.markdown("<div class='subtitle' style='font-size:24px;>CSE Student at PES University</div>", unsafe_allow_html=True)
+    st.markdown("<div class='subtitle'>CSE Student at PES University</div>", unsafe_allow_html=True)
     st.markdown("""
 <p style='font-size:18px; line-height:1.6;'>
 Final-year Computer Science student at PES University, passionate about Machine Learning and Natural Language Processing. 
