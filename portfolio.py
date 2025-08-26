6b066fb3fef46174367309f00d0f1fe35f961833
Suhas Venkata <113611352+sUhAs1011@users.noreply.github.com>
2025-08-26 10:00:32 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1059,6 +1059,9 @@ st.markdown("""
     <div style='text-align: center; padding-top: 20px; font-size: 40px; font-weight: 500; color: #ffffff;'>
         Made by Suhas Venkata
     </div>
+    <div style='text-align: center; padding-top: 20px; font-size: 20px; font-weight: 500; color: #ffffff;'>
+        Using Python And Streamlit
+    </div>
 """, unsafe_allow_html=True)
 
 
