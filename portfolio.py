9141892626b71c77d74373bc6144bfec9ddecda6
Suhas Venkata <113611352+sUhAs1011@users.noreply.github.com>
2025-09-01 12:51:36 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -4,7 +4,7 @@ import base64
 from io import BytesIO
 
 # --- PAGE CONFIG ---
-st.set_page_config(page_title="Suhas Venkata · Portfolio", page_icon="suhas1.jpg", layout="wide")
+st.set_page_config(page_title="Suhas Venkata · Portfolio", page_icon="suhas.jpg", layout="wide")
 
 # --- LOAD IMAGE ---
 def load_and_base64_image(file_path):
