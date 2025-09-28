4f2d308ed9d0888d2d47cb6af4f26b631b1167e1
Suhas Venkata <113611352+sUhAs1011@users.noreply.github.com>
2025-09-28 18:04:09 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -155,7 +155,7 @@ body {
 }
 
 .profile-pic {
-    border-radius: 12px;
+    border-radius: 50%;
     width: 300px;
     height: 300px;
     object-fit: cover;
@@ -189,7 +189,7 @@ body {
         display: block;
         object-fit: cover;
         object-position: center;
-        border-radius: 12px;
+        border-radius: 50%;
         overflow: hidden;
     }
     
@@ -225,7 +225,7 @@ body {
         height: 150px;
         object-fit: cover;
         object-position: center;
-        border-radius: 12px;
+        border-radius: 50%;
         overflow: hidden;
     }
     
