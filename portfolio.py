4b020c4b461f4fb63d309d816fbba3ed9594ddec
Suhas Venkata <113611352+sUhAs1011@users.noreply.github.com>
2025-10-07 15:22:11 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1033,7 +1033,7 @@ ml_icon_map = {
     "Keras": "https://img.icons8.com/?size=100&id=XcSgtbIpgK6W&format=png&color=000000",
     "Seaborn": "https://cdn.worldvectorlogo.com/logos/seaborn-1.svg",
     "Selenium" : "https://img.icons8.com/?size=100&id=38553&format=png&color=000000",
-    "LLM": https://img.icons8.com/?size=100&id=29a6ubG1s7cw&format=png&color=000000
+    "LLM": "https://img.icons8.com/?size=100&id=29a6ubG1s7cw&format=png&color=000000"
 }
 
 ml_tools = ["Scikit-learn", "Pandas", "NumPy", "NLTK", "Spacy", "PyTorch", "Mathplotlib", "Keras", "Seaborn", "Selenium","LLM"]
