6d1a54e10caf35fcfc462f690ec952f7e110fa44
Suhas Venkata <113611352+sUhAs1011@users.noreply.github.com>
2025-11-05 19:53:31 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1497,6 +1497,40 @@ with col_right:
     st.image("node_simulator.jpg", caption="Distributed Systems Cluster Simulator", use_container_width=True)
 
 
+# Detection And Mitigation Of Replay Attack in CCTV systems
+st.markdown("---")
+col_left, col_right = st.columns([1, 2])
+
+with col_left:
+    st.image("cctv.jpg", caption="Replay Attack Detection And Mitigation", use_container_width=True)
+
+with col_right:
+    st.markdown(
+        """
+        <h3>
+            <img src='https://img.icons8.com/?size=100&id=3wl52ZDVBgG0&format=png&color=000000' 
+                 style='width:28px; height:26px; vertical-align:middle; margin-right:8px;'/>
+            <a href='https://github.com/sUhAs1011/Capstone_Project' 
+               target='_blank' style='text-decoration: none; color: white;'>
+               Detection And Mitigation Of Replay Attack in CCTV systems
+            </a>
+        </h3>
+        """,
+        unsafe_allow_html=True
+    )
+    st.write("""
+    Replay attacks on CCTV systems exploit vulnerabilities by retransmitting recorded footage to bypass live monitoring
+
+    Key features include:
+    - *Optical flow–based motion analysis* with *SDR* encoding to capture temporal motion patterns in a compact and interpretable form.
+    - *Hierarchical Temporal Memory (HTM)* model for lightweight, real-time anomaly detection of unusual motion sequences.
+    - *SHA‑256* frame hashing and verification to ensure tamper-evident, forensic reliability of CCTV footage.
+    - Decision engine with dashboard interface that fuses anomaly scores and integrity checks, providing real-time alerts, visualization, and forensic reporting.
+    
+    Tech Stack: Python, HTM, SHA-256, SDR
+    """)
+
+
 # --- Custom Footer with Styling ---
 st.markdown("""
     <div style='text-align: center; padding-top: 20px; font-size: 40px; font-weight: 500; color: #ffffff;'>
